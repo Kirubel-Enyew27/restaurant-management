2225bda9e7e6d6bf289580cc03e8c1131a226030
Kira37 <kirubelenyew76@gmail.com>
2025-01-27 11:38:03 +0300

    add customer update request dto

---
+++ internal/constant/model/dto/customer.go
@@ -0,0 +1,10 @@
+package dto
+
+import "github.com/google/uuid"
+
+type UpdateRequest struct {
+	UserID   uuid.UUID `json:"user_id"`            // Required field
+	Username string    `json:"username,omitempty"` // Optional field, use *string for nullability
+	Email    string    `json:"email,omitempty"`    // Optional field, use *string for nullability
+	Password string    `json:"password,omitempty"` // Optional field, use *string for nullability
+}
