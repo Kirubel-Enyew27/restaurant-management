2d6245560fa7f05610becdbbb93e3baa296ea927
Kira37 <kirubelenyew76@gmail.com>
2025-01-31 17:21:02 +0300

    modify order dto

--- internal/constant/model/dto/order.go
+++ internal/constant/model/dto/order.go
@@ -2,22 +2,23 @@ package dto
 
 import (
 	"database/sql"
+	"restaurant/internal/constant/model/db"
 
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
 )
 
 type OrderItem struct {
-	OrderItemID uuid.UUID       `json:"order_item_id,omitempty"`
-	OrderID     uuid.UUID       `json:"order_id,omitempty"`
-	MealID      uuid.UUID       `json:"meal_id,omitempty"`
-	Quantity    int32           `json:"quantity,omitempty"`
+	OrderItemID uuid.NullUUID   `json:"order_item_id,omitempty"`
+	OrderID     uuid.NullUUID   `json:"order_id,omitempty"`
+	MealID      uuid.NullUUID   `json:"meal_id,omitempty"`
+	Quantity    sql.NullInt32   `json:"quantity,omitempty"`
 	Price       decimal.Decimal `json:"price,omitempty"`
 }
 
 type CreateOrderRequest struct {
-	OrderID     uuid.UUID       `json:"order_id,omitempty"`
-	UserID      uuid.UUID       `json:"user_id,omitempty"`
+	OrderID     uuid.NullUUID   `json:"order_id,omitempty"`
+	UserID      uuid.NullUUID   `json:"user_id,omitempty"`
 	OrderStatus string          `json:"order_status,omitempty"`
 	TotalPrice  decimal.Decimal `json:"total_price,omitempty"`
 	Item        []OrderItem
@@ -25,25 +26,10 @@ type CreateOrderRequest struct {
 
 type OrderResponse struct {
 	OrderID     uuid.UUID       `json:"order_id,omitempty"`
-	OrderStatus string          `json:"order_status,omitempty"`
+	OrderStatus sql.NullString  `json:"order_status,omitempty"`
 	TotalPrice  decimal.Decimal `json:"total_price,omitempty"`
-	User        User
+	User        db.User
 	OrderItem   []OrderItem
-	Meal        Meal
 	CreatedAt   sql.NullTime
 	ModifiedAt  sql.NullTime
-}
-
-type User struct {
-	UserID   uuid.UUID `json:"user_id"`
-	Username string    `json:"username"`
-	Email    string    `json:"email"`
-	Password string    `json:"password,omitempty"`
-}
-
-type Meal struct {
-	MealID    uuid.UUID       `json:"meal_id"`
-	Name      string          `json:"name"`
-	Price     decimal.Decimal `json:"price"`
-	Available *bool           `json:"available,omitempty"`
 }
