78015e7f89243740e3ff94c0df42216045bd5561
Kira37 <kirubelenyew76@gmail.com>
2025-02-01 10:07:03 +0300

    implement handler layer of create order

--- internal/handler/order/order.go
+++ internal/handler/order/order.go
@@ -1,6 +1,11 @@
 package order
 
 import (
+	"context"
+	"net/http"
+	"restaurant/internal/constant/errors"
+	"restaurant/internal/constant/model/dto"
+	"restaurant/internal/constant/model/response"
 	"restaurant/internal/handler"
 	"restaurant/internal/service"
 	"time"
@@ -27,5 +32,25 @@ func Init(log *zap.Logger, orderModule service.Order,
 	}
 }
 
-func (o *order) CreateOrder(ctx *gin.Context) {}
-func (o *order) GetOrders(ctx *gin.Context)   {}
+func (o *order) CreateOrder(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), o.contextTimeout)
+	defer cancel()
+
+	var req dto.CreateOrderRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		err := errors.ErrBadRequest.Wrap(err, "failed to bind request body")
+		o.logger.Info("invalid request body", zap.Error(err))
+		_ = c.Error(err)
+		return
+	}
+
+	order, err := o.orderModule.CreatedOrder(ctx, req)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	response.SendSuccessResponse(c, http.StatusCreated, order, nil)
+}
+func (o *order) GetOrders(ctx *gin.Context) {}
