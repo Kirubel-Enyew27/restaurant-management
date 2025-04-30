3d91b798bb1c89b9ee6fd3fb0edcde5c8d6c609f
Kira37 <kirubelenyew76@gmail.com>
2025-04-30 10:38:42 +0300

    modified Cors Middleware

--- internal/handler/middleware/CORS.go
+++ internal/handler/middleware/CORS.go
@@ -2,13 +2,21 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+
+		if origin == allowedOrigin {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+			c.Writer.Header().Set("Vary", "Origin") // For proxies/caching
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
