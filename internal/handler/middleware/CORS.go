378240a5e34c79067e79c58f4880a3fac694e023
Kira37 <kirubelenyew76@gmail.com>
2025-06-18 16:37:52 +0300

    removed Access-Control-Allow-Credentials: true

--- internal/handler/middleware/CORS.go
+++ internal/handler/middleware/CORS.go
@@ -11,7 +11,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
