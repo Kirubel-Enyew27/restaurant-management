f405e143453b244dbb745052b35884f774f99de6
Kira37 <kirubelenyew76@gmail.com>
2025-06-18 16:25:14 +0300

    apply open CORS temporarily to confirm

--- internal/handler/middleware/CORS.go
+++ internal/handler/middleware/CORS.go
@@ -8,20 +8,9 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			c.Writer.Header().Set("Vary", "Origin")
-		}
-
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-
-		if requestedHeaders := c.GetHeader("Access-Control-Request-Headers"); requestedHeaders != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Headers", requestedHeaders)
-		} else {
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		}
-
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == http.MethodOptions {
